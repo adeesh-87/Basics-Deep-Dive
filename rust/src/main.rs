46384c4600692c31dfc36072867354768e387864
= <=>
2019-08-01 00:37:31 +0530

    Quick Sort for Arrays

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,7 @@
+mod quick_sort;
+
 fn main() {
-    println!("Rust Upppp !!!!!");
+    let mut array = [7,3,2,8,1,48,55,12,24,15,24,21,15];
+    quick_sort::quick_sort(&mut array,0,12);
+    println!("{:?}",array)
 }
