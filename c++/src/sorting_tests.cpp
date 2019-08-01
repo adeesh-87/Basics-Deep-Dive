4b95046ab20872f29e92cc0aa9cf28c618dca34a
adeesh <adeesh.kale@gmail.com>
2019-08-01 15:25:03 +0530

    Some polishing of the test suite with Lomuto quicksorting

--- c++/src/sorting_tests.cpp
+++ c++/src/sorting_tests.cpp
@@ -4,35 +4,89 @@
 #include <bits/stdc++.h>
 
 
-const int rand_array_size = 100;
-int rand_array[rand_array_size];
-int rand_array_copy[rand_array_size];
-int *rand_array_addr, *rand_array_copy_addr;
 
+volatile q_sort_schema_t q_sort_partition_style;
+int num_tests, num_tests_pass;
+
+void generate_test_array(int * x_addr, int * x_ref_addr, int size, int val_limit);
+bool check_arrays_equal(int x[], int y[], int size);
+void tester_with_rand_generator(void (*sorter)(int * p_array, int array_size), int test_array_size);
+void print_array(int arr[], int size);
 
 using namespace std;
 
 int main() {
 	srand(time(NULL));
+	cout << "**********************************************************************" << endl;
+	cout << "                      Executing Tests                                 " << endl;
+	cout << "**********************************************************************" << endl;
 	
+	// LOMUTO tests
+	q_sort_partition_style = LOMUTO;
+	tester_with_rand_generator(q_sort_start, 10);
+	tester_with_rand_generator(q_sort_start, 23);
+	tester_with_rand_generator(q_sort_start, 42);
+	tester_with_rand_generator(q_sort_start, 200);
+	tester_with_rand_generator(q_sort_start, 500);
+	tester_with_rand_generator(q_sort_start, 1000);
+	tester_with_rand_generator(q_sort_start, 10000);
+	cout << "**********************************************************************" << endl;
+	cout << "                      Lomuto Test Results                             " << endl;
+	cout << "**********************************************************************" << endl;
+	cout << "Number of tests = " << num_tests << " Passed = " << num_tests_pass  << " Failed = " << (num_tests - num_tests_pass) << endl;
 	
+	return 0;
+}
 
-	sort(rand_array_copy_addr, (rand_array_copy_addr+rand_array_size));
-	q_sort((rand_array_addr), 0, (rand_array_size-1));
-	int check_var;
-	
-	for(int i = 0; i<rand_array_size; i++)
+void tester_with_rand_generator(void (*sorter)(int * p_array, int array_size), int test_array_size) {
+	int rand_array[test_array_size];
+	int rand_array_reference[test_array_size];
+	int *rand_array_addr = rand_array, *rand_array_reference_addr = rand_array_reference;
+	int result;
+	num_tests++;
+	for (int i = 0; i < test_array_size; i++) {
+		rand_array[i] = rand() % 100;
+		rand_array_reference[i] = rand_array[i];
+	}
+	sort(rand_array_reference_addr, (rand_array_reference_addr+test_array_size));
+	sorter((rand_array_addr), (test_array_size-1));
+	result = ( check_arrays_equal(rand_array_addr, rand_array_reference_addr, test_array_size) ? 1 : 0 );
+	cout << "Test " << num_tests << " ";
+	if(result)
 	{
-		if(rand_array[i] != rand_array_copy[i])
+		num_tests_pass++;
+		cout << "Pass" << endl;
+	} else {
+		cout << "Fail" << endl;
+	}
+	/* Did just for confirmation
+	if(test_array_size < 50) {
+		print_array(rand_array, test_array_size);
+		print_array(rand_array_reference, test_array_size);
+	}*/
+}
+
+void print_array(int arr[], int size) {
+	cout << "Array: " << arr[0];
+	for (int i = 0; i < size; i++) {
+		cout << ", " << arr[i];
+	}
+	cout << endl;
+}
+
+bool check_arrays_equal(int x[], int y[], int size) {
+	int check_var = 0;
+	
+	for (int i = 0; i<size; i++)
+	{
+		if (x[i] != y[i])
 		{
 			check_var++;
 		}
 	}
-	if(check_var) {
-		cout << "Fail :(!" << endl;
-	} else {
-		cout << "Success!" << endl;
+	if (check_var) {
+		return false;
 	}
-	return 0;
+	
+	return true;
 }
-
