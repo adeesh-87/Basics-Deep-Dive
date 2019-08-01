e622f25b33209c13e4a615d681bfe70516963031
adeesh <adeesh.kale@gmail.com>
2019-08-01 16:10:23 +0530

    bugfix in rust quick_sort

--- rust/src/quick_sort.rs
+++ rust/src/quick_sort.rs
@@ -1,23 +1,22 @@
 pub fn quick_sort(array: &mut [isize], left: usize, right: usize) {
-    if left < right {
-        let pivot = array[right];
-        let mut i = left;
-        let mut j = left;
-
-        while j < right - 1 {
-            if array[j] < pivot {
-                array.swap(i, j);
-                i += 1;
-            }
-            j += 1;
-        }
-
-        if array[right] < array[i] {
-            array.swap(i, right);
-        }
-
-        quick_sort(array, left, i); 
-        quick_sort(array, i + 1, right);
-    }
+	if left < right {
+		let pivot = array[right];
+		let mut i = left;
+		let mut j = left;
+		while j < right {
+			if array[j] < pivot {
+				array.swap(i, j);
+				i += 1;
+			}
+			j += 1;
+		}
+		array.swap(i, right);
+		if i > 0 {
+			quick_sort(array, left, i - 1);
+		}
+		if i < right {
+			quick_sort(array, i + 1, right);
+		}
+	}
 }
 
