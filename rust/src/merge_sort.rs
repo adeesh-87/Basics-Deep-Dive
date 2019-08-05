4fc457964abab047340462c74434c2fa30cac80b
= <=>
2019-08-05 16:58:43 +0530

    Test cases - Passing Bug fixced - vector split_to_mut splits the vector from src

--- rust/src/merge_sort.rs
+++ rust/src/merge_sort.rs
@@ -1,32 +1,42 @@
 fn merge(vec:&mut Vec<usize>, left:usize, mid: usize, right: usize) {
-    let (first_half, second_half) = vec.split_at_mut(mid);
-    let mut first_half = first_half.to_vec();
-    let mut second_half = second_half.to_vec();
-    for i in left..right {
+    println!("begin {:?} mid {} ",vec,mid);
+    let mut first_half = vec[left..mid+1].to_vec();
+    let mut second_half = vec[mid+1..right+1].to_vec();
+    first_half.reverse();
+    println!("FH {:?} ",first_half);    
+    second_half.reverse();
+    println!("SH {:?} ",second_half);    
+    for i in left..right+1 {
         // check if first half empty remove the begin from second half to the vector else complier panics 
         if first_half.is_empty() {
-            vec[i] = second_half.remove(0);
+            println!("In FHE {:?} ",vec[i]);
+            vec[i] = second_half.pop().unwrap();
             continue; 
         }
         // likewise for the second half
         if second_half.is_empty() {
-            vec[i] = first_half.remove(0);
+            println!("In SHE {:?} ",vec[i]);
+            vec[i] = first_half.pop().unwrap();
             continue; 
         }
 
-        if second_half.first() < first_half.first() {
-            vec[i] = second_half.remove(0)
+        if second_half.last() < first_half.last() {
+            vec[i] = second_half.pop().unwrap();
+            println!("In Vec Swap {:?} ",vec[i]);
         } else {
-            vec[i] = first_half.remove(0)
+            vec[i] = first_half.pop().unwrap();
+            println!("In Vec Swap {:?} ",vec[i]);
         }
     }
+    println!("End {:?} ",vec);
 }
 
 pub fn merge_sort(vec: &mut Vec<usize>, left: usize, right: usize) {
     if left < right {
         let mid: usize = (left + right)/2;
+        println!("{}",mid);
         merge_sort(vec, left, mid);
-        merge_sort(vec, mid + 1, right);
+        merge_sort(vec, mid+1, right);
         merge(vec, left, mid, right);
     }
 }
@@ -42,10 +52,10 @@ mod tests {
         assert_eq!(vec,[1,2,4,8,9]);
     }
 
-    
+    #[test]    
     fn merge_sort_test_2(){
         let mut vec = vec![4,9,2,8,1,54,44,43,84];
-         merge_sort(&mut vec,0,8);
+        merge_sort(&mut vec,0,8);
         assert_eq!(vec,[1,2,4,8,9,43,44,54,84]);
     }
 }
