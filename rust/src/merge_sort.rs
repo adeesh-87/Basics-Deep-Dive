cf84af8ffdf6b6052bd7660cf862ec9108ee2626
= <=>
2019-08-05 16:01:07 +0530

    Test Cases for Merge Sort - test failing

--- rust/src/merge_sort.rs
+++ rust/src/merge_sort.rs
@@ -31,3 +31,21 @@ pub fn merge_sort(vec: &mut Vec<usize>, left: usize, right: usize) {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn merge_sort_test_1(){
+        let mut vec = vec![4,9,2,8,1];
+        merge_sort(&mut vec,0,4);
+        assert_eq!(vec,[1,2,4,8,9]);
+    }
+
+    
+    fn merge_sort_test_2(){
+        let mut vec = vec![4,9,2,8,1,54,44,43,84];
+         merge_sort(&mut vec,0,8);
+        assert_eq!(vec,[1,2,4,8,9,43,44,54,84]);
+    }
+}
