0f55a629774d60ea0fd5f896e0d1b27223450cef
= <=>
2019-08-05 17:03:33 +0530

    Read User Input for Merge Sort

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -21,6 +21,25 @@ fn quick_sort() {
     }
 }
 
+fn merge_sort() {
+    let mut vec_value: Vec<usize> = Vec::new();
+    let mut buf = String::new(); 
+    println!("Enter no of variables for Sort ");
+    io::stdin().read_line(&mut buf).expect("Failed to Read line");
+    let n: usize = buf.trim().parse::<usize>().expect("Invalid Input");
+    for _i in 0..n {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).expect("Failed to Read line");
+        let num: usize = buf.trim().parse::<usize>().expect("Invalid Input");
+        vec_value.push(num);
+    } 
+    merge_sort::merge_sort(&mut vec_value,0,(n - 1) as usize);
+
+    for v in vec_value.iter() {
+        println!("{}",v )
+    }
+}
+
 fn main() {
-    quick_sort();
+    merge_sort();
 }
